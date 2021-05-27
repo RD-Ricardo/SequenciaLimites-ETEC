863f31c9e861915d2c2042be7309c1fb51747e9e
RD-Ricardo <ricardodias169@gmail.com>
2021-05-27 15:26:15 -0300

    Finalizando Programa

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SequenciaLimites
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine("--Sequencia Limites--");
+            Console.WriteLine("---------------------\n");
+            Console.Write("Inicio...: ");
+            int inicio = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Fim...: ");
+            int fim = Convert.ToInt32(Console.ReadLine());
+            if(inicio >= fim)
+            {
+                Console.Write("Deu erro!! o Campo Inicio não pode ser maior que o Fim!!");
+            }
+            else
+            {
+                for( int i = inicio; i <=fim ; i++)
+                {
+                    Console.WriteLine(i);    
+                }
+            }
+            
+
+        }
+    }
+}
